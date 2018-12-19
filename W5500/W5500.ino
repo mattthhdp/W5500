59462def96c254a7e912eeb5953a31be170b9ace
Jaune <mattthhdp@hotmail.com>
2018-12-19 07:14:28 -0500

    Add project files.

---
+++ W5500/W5500.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		W5500.ino
+ Created:	12/19/2018 7:14:14 AM
+ Author:	mattt
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
