9d8f507fc921c61155ddec5d0b75d2bff0ee3813
Jaune <mattthhdp@hotmail.com>
2018-12-22 00:30:13 -0500

    Ajout de la library Etherner2 ... J'ai un W5500 Ajout de MQTT Ajout de Ethernet Ajour de debugging Ajout d'adress Static et dynamic Ajout Adress Mac Static et Dynamic

--- W5500/W5500.ino
+++ W5500/W5500.ino
@@ -6,28 +6,173 @@
 
 
 #include "configuration.h"
-//#include <Ethernet.h>
-byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 }; //physical mac address
+#include <SPI.h>                  // For networking
+#include <Ethernet2.h>             // For networking
+#include <PubSubClient.h>         // For MQTT
+#include "Wire.h"                 // For MAC address ROM
 
 
-int output_pin[20] = { A0, A1, A2, A3, A4, A5,0,1,2,3,4,5,6,7,8,9,10,11,12,13 };
-int output_state[20] = { 0,  0,  0,  0,  0,  0,  0,  0,0,0,0,0,0,0,0,0,0,0,0,0 };
+//Configuration //
+#define Enable_Dhcp               false   // true/false
+IPAddress ip(192, 168, 1, 35);                //Static Adress if Enable_Dhcp = false
+
+#define Enable_Mac_Address_Rom    false   // true/false
+static uint8_t mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE };  // Set if there is no Mac_room
+#define MAC_I2C_ADDRESS             0x50   // Microchip 24AA125E48 I2C ROM address
+
+
+// MQTT Settings //
+IPAddress broker(192, 168, 1, 111);        // MQTT broker
+const char* statusTopic = "events";    // MQTT topic to publish status reports
+char messageBuffer[100];
+char topicBuffer[100];
+char clientBuffer[50];
+
+//Relay Pinout //
+int output_pin[20] = { A0, A1, A2, A3, A4, A5, 0, 1, 2, 3,
+                        4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+int output_state[20] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 const int output_number_pin = 20;
+
+#pragma region Setup
+
+/**
+ * MQTT callback
+ */
+void callback(char* topic, byte* payload, unsigned int length)
+{
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	Serial.print("] ");
+	for (int i = 0; i < length; i++) {
+		Serial.print((char)payload[i]);
+	}
+	Serial.println();
+}
+
+// Instantiate MQTT client
+//PubSubClient client(broker, 1883, callback);
+EthernetClient ethclient;
+PubSubClient client(ethclient);
+
+void reconnect() {
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection to : ");
+		Serial.println(broker);
+		// Attempt to connect
+		String clientString = "Reconnecting Arduino-" + String(Ethernet.localIP());
+		clientString.toCharArray(clientBuffer, clientString.length() + 1);
+		if (client.connect(clientBuffer)) {
+			Serial.println("connected");
+			// Once connected, publish an announcement...
+			clientString.toCharArray(clientBuffer, clientString.length() + 1);
+			client.publish(statusTopic, clientBuffer);
+			// ... and resubscribe
+			//client.subscribe("inTopic");
+		}
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+
+#pragma region Debugging MQTT error
+
+			if (client.state() == -1)
+			{
+				Serial.print(" Timed_out");
+			}
+			else if (client.state() == -2)
+			{
+				Serial.print(" INVALID_SERVER");
+			}
+			else if (client.state() == -3)
+			{
+				Serial.print(" TRUNCATED");
+			}
+			else if (client.state() == -4)
+			{
+				Serial.print(" INVALID_RESPONSE");
+			}
+			else
+				Serial.print(" Impossible Error ... lol"); 
+#pragma endregion
+			Serial.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
+}
+
+#pragma endregion
 
 
 void setup() 
 	{
-		enable_and_reset_all_outputs();
-		
-		sprintf(MQTTBROKER, "Test ip");
+	Serial.begin(115200);
+	Serial.println();
+	Serial.println("=====================================");
+	Serial.println("Starting up OutputBoard Relay W5500 v1.0");
+	Serial.println("=====================================");
+
+#pragma region Mac and ip Setup
+
+	if (Enable_Mac_Address_Rom == true)
+	{
+		Serial.print(F("Getting MAC address from ROM: "));
+		mac[0] = readRegister(0xFA);
+		mac[1] = readRegister(0xFB);
+		mac[2] = readRegister(0xFC);
+		mac[3] = readRegister(0xFD);
+		mac[4] = readRegister(0xFE);
+		mac[5] = readRegister(0xFF);
+	}
+	else {
+		Serial.print(F("Using static MAC address: "));
+	}
+	// Print the IP address
+	char tmpBuf[17];
+	sprintf(tmpBuf, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	Serial.println(tmpBuf);
+
+	// Set up the Ethernet library to talk to the Wiznet board
+	if (Enable_Dhcp == true)
+	{
+		Serial.print("Using Dhcp : ");
+		Ethernet.begin(mac);      // Use Dhcp
+	}
+	else {
+		Serial.print("Using Static IP : ");
+		Ethernet.begin(mac, ip);  // Use static address defined above
+	}
+
+	Serial.println(Ethernet.localIP());
+	Serial.println("=====================================");
+
+
+#pragma endregion
+	enable_and_reset_all_outputs(); //Reset and Set all pin on OUTPUT mode
+
+
+	client.setServer(broker, 1883);
+	client.setCallback(callback);
+	String clientString = "Starting Arduino-" + Ethernet.localIP();
+	clientString.toCharArray(clientBuffer, clientString.length() + 1);
+	client.publish(statusTopic, clientBuffer);
+	reconnect();
+
 	}
 void loop() 
 	{
-	//turn_output_off_on();
-	sprintf(MQTTBROKER, "Test ip");
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+
+
 	}
 
-
+/// Set all pint to OUTPUT and state to 0 = OFF
 void enable_and_reset_all_outputs()
 {
 	for (int i = 0; i < output_number_pin; i++)
@@ -37,8 +182,6 @@ void enable_and_reset_all_outputs()
 		output_state[i] = 0;
 	}
 }
-
-
 
 void turn_output_off(int output_number)
 {
@@ -85,29 +228,18 @@ void turn_output_on(int output_number)
 	Serial.println(message);
 }
 
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-	Serial.print("Message  [");
-	Serial.print(topic);
-	Serial.print("] ");
-	for (int i = 0; i < length; i++) {
-		Serial.print((char)payload[i]);
-	}
-	Serial.println();
+byte readRegister(byte r)
+{
+	unsigned char v;
+	Wire.beginTransmission(MAC_I2C_ADDRESS);
+	Wire.write(r);  // Register to read
+	Wire.endTransmission();
 
-	byte output_number = payload[0] - '0';
-	byte output_state = payload[2] - '0';
-	Serial.print("Output: ");
-	Serial.println(output_number);
-	Serial.print("State: ");
-	Serial.println(output_state);
-
-	switch (output_state)
+	Wire.requestFrom(MAC_I2C_ADDRESS, 1); // Read a byte
+	while (!Wire.available())
 	{
-	case 0:
-		turn_output_off(output_number);
-		break;
-	case 1:
-		turn_output_on(output_number);
-		break;
+		// Wait
 	}
+	v = Wire.read();
+	return v;
 }
